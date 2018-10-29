5bc7e6a90be37b5bd8594111de47de8877a3c821
Jonathan Deleon <Jondeleon856@gmail.com>
2018-10-29 09:10:49 -0400

    added gulp sass

---
+++ gulpfile.js
@@ -0,0 +1,18 @@
+'use strict';
+ 
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+ 
+sass.compiler = require('node-sass');
+ 
+gulp.task('sass', function () {
+  return gulp.src('./sass/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./css'));
+});
+
+//Watch tasks need to have series attached to it
+gulp.task('default', function () {
+  gulp.watch('sass/**/*.scss', gulp.series('sass'));
+
+});
