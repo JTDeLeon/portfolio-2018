1a3ecd7502d781822b529ac17b2f8134ff883cff
Jonathan Deleon <Jondeleon856@gmail.com>
2018-10-31 15:18:38 -0400

    added menu and started about section

---
+++ app/js/menu.js
@@ -0,0 +1,4 @@
+//@TODO Need to connect this to the site and edit the gulp file to manage these js files.
+
+const mobMenu = document.querySelector('mobile-menu-link-container');
+console.log(mobMenu);
